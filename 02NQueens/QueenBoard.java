d75b47946995cac91a3edaf25e051fee2ff413b9
jt1250champ <jtao@stuy.edu>
2017-02-13 02:14:10 -0500

    had to redo queenboard too

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -4,6 +4,11 @@ public class QueenBoard{
     
     public QueenBoard(int size){
 	board = new int[size][size];
+	for(int row = 0; row < size; row++) {
+	    for(int col = 0; col < size; col++) {
+		board[row][col] = 0;
+	    }
+	}
     }
 
     /**
@@ -21,9 +26,29 @@ public class QueenBoard{
     }
 
     private boolean solveH(int col){
-	return false;
+	if(col >= board[0].length) {
+	    return false;
+	}else {
+	    for(int row = 0; row < board[0].length; row++) {
+		if(board[row][col] == 0) {
+		    
+		}
+	    }
+	}
     }
 
+    private boolean addQueen(int row, int col) {
+	board[row][col] = -1;
+	for (int i = 0; i < board[0].length; i++) {
+	    if(board[row][i] != -1) {
+		board[row][i] += 1;
+	    }
+	}
+    }
+
+    private boolean removeQueen(int row, int col) {
+	
+    }
     /**
      *@return the number of solutions found, or -1 if the board was never solved.
      *The board should be reset after this is run.    
