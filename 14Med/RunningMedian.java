859a45b35cabbb438fec5ad7f7f77d78ad59e746
jt1250champ <jtao@stuy.edu>
2017-05-09 00:28:28 -0400

    needs further testing but appears to be working

--- 14Med/RunningMedian.java
+++ 14Med/RunningMedian.java
@@ -4,27 +4,46 @@ public class RunningMedian {
 
     private MyHeap lower;
     private MyHeap higher;
+    private int sizeL, sizeH, med1, med2 = 0;
 
     public RunningMedian() {
 	lower = new MyHeap(true);
 	higher = new MyHeap(false);
     }
 
-    public void add(int n) {
-	if(lower.size() < higher.size()) {
+    public void add(int n){
+	if(sizeL == 0 && sizeH == 0){
 	    lower.add(n);
-	}else {
-	    higher.add(n);
+	    sizeL++;
+	}else{
+	    if(n >= getMedian()){
+		higher.add(n);
+		sizeH++;
+	    }else if(n < getMedian()){
+		lower.add(n);
+		sizeL++;
+	    }
+	}
+
+	//balancing stuff
+	if(sizeL + 1 < sizeH) {
+	    lower.add(higher.remove());
+	    sizeH++;
+	    sizeL--;
+	}else if(sizeH + 1 < sizeL) {
+	    higher.add(lower.remove());
+	    sizeH--;
+	    sizeL++;
 	}
     }
 
     public double getMedian() {
-	if(lower.size() < higher.size()) {
-	    return higher.peek();
-	}else if(lower.size() > higher.size()) {
+	if(sizeL == sizeH) {
+	    return (double)(higher.peek() + lower.peek()) / 2;
+	}else if(sizeL > sizeH) {
 	    return lower.peek();
 	}else{
-	    return (higher.peek() + lower.peek()) / 2;
+	    return higher.peek();
 	}
     }
 }
