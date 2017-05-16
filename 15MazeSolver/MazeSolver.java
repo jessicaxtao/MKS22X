98a42c6f586845f521d807e0bf8e60f585acfde8
jt1250champ <jtao@stuy.edu>
2017-05-16 11:49:53 -0400

    uh started

---
+++ 15MazeSolver/MazeSolver.java
@@ -0,0 +1,9 @@
+public class MazeSolver {
+    private class Location {
+	private int row, col;
+	private Location previous;
+	private int distanceToStart;
+    }
+    private class FrontierPriorityQueue {
+    }
+}
