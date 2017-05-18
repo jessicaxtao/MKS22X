5960459f6c854b4334289106ce086f50334a682b
jt1250champ <jtao@stuy.edu>
2017-05-18 12:26:28 -0400

    ^^

--- 15MazeSolver/MazeSolver.java
+++ 15MazeSolver/MazeSolver.java
@@ -1,8 +1,28 @@
 public class MazeSolver {
-    public MazeSolver(String m) {
+
+    public MazeSolver(String fileything) {
+	this(fileything, false);
     }
+
+    public MazeSolver(String fileything, boolean animate) {
+    }
+
     public void solve() {
+	solve(1);
     }
-    public void solve(int a) {
+
+    public void solve(int style) {
+	if(style == 0) {
+	    //DFS
+	}else if(style == 1) {
+	    //BFS
+	}else if(style == 2) {
+	    //BestFirst
+	}else if(style == 3) {
+	    //A*
+	}
+    }
+
+    public String toString() {
     }
 }
